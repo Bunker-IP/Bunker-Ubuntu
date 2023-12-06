667103ef372bb566204f64c655d62b20f0176407
y2k <66226195+unixfool@users.noreply.github.com>
2023-12-06 21:56:38 +0100

    Create pre-run.sh

---
+++ pre-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "We going to install: dig, whois, net-tools, ufw, build-essential";
+
+sudo apt install dig whois net-tools ufw build-essential
+
+echo "";
+echo "All packets are installet. You don't need to run this again!"
+echo "";
+echo "Going to remove pre-run.sh"
+
+sudo rm -rf pre-run.sh
+exit
