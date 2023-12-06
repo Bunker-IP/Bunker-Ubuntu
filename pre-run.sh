fb94557b10c18eef9a641650533ef9b13fd143ad
y2k <66226195+unixfool@users.noreply.github.com>
2023-12-06 21:25:01 +0100

    Delete pre-run.sh

--- pre-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt get install dig whois net-tools ufw build-essential
-echo "Finish Install Packets"
-# This is going to install the packets for you. Only need to run this script 1 time.
